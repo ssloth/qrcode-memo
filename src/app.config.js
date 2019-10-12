b3a41ac2246ac8c78d62e68152750075dfb798b7
ZY <1035041075@qq.com>
2019-10-12 20:38:26 +0800

    init:use remax

---
+++ src/app.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  pages: ['pages/index/index'],
+  window: {
+    navigationBarTitleText: '扫码备注',
+    navigationBarBackgroundColor: '#282c34'
+  }
+};
---
+++ src/pages/index/index.js
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import { scanCode, cloud } from 'remax/wechat'
+import { View, Text, Image, showModal } from 'remax/wechat'
+import styles from './index.module.css'
+
+const { useEffect } = React
+
+export default () => {
+  const handleClick = () => {
+    scanCode().then(res => {
+      showModal({
+        title: res
+      })
+    })
+  }
+
+  useEffect(() => {
+    // cloud.callFunction({
+    //   name: 'login'
+    // })
+  }, [])
+
+  return (
+    <View className={styles.app}>
+      <View onClick={handleClick} className={styles.header}>
+        <Image
+          src='https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ'
+          className={styles.logo}
+          alt='logo'
+        />
+        <View className={styles.text}>
+          编辑 <Text className={styles.path}>src/pages/index/index.js</Text>开始
+        </View>
+      </View>
+    </View>
+  )
+}
